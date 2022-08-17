dfadf38eb8d3f108b3c6d8729d80e044cfe3d3c4
Michele Riva <ciao@micheleriva.it>
2022-08-17 13:41:11 +0200

    refactor: makes common methods runtime-agnostic

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,5 +1,10 @@
+import type { PropertiesSchema, Lyra } from '@nearform/lyra'
 import type { PersistenceFormat, AvailableRuntimes } from './types'
-import { DPACK_NOT_SUPPORTED_ON_DENO } from './errors'
+import { save, create } from '@nearform/lyra'
+// @ts-expect-error dpack does not expose types
+import { serialize, parse } from 'dpack'
+import { encode, decode } from '@msgpack/msgpack'
+import { DPACK_NOT_SUPPORTED_ON_DENO, UNSUPPORTED_FORMAT } from './errors'
 
 export const DEFAULT_DB_NAME = `lyra_bump_${+new Date()}`
 
@@ -23,4 +28,55 @@ export function getDefaultFileName (format: PersistenceFormat, runtime: Availabl
   const dbName = process?.env?.LYRA_DB_NAME || DEFAULT_DB_NAME
 
   return `${dbName}.${extension}`
+}
+
+export function persist<T extends PropertiesSchema> (db: Lyra<T>, format: PersistenceFormat = 'binary'): string | Buffer {
+  const dbExport = save(db)
+  let serialized: string | Buffer
+
+  switch (format) {
+    case 'json':
+      serialized = JSON.stringify(dbExport)
+      break
+    case 'dpack':
+      serialized = serialize(dbExport)
+      break
+    case 'binary':
+      const msgpack = encode(dbExport)
+      serialized = Buffer.from(msgpack.buffer, msgpack.byteOffset, msgpack.byteLength)
+      break
+    default:
+      throw new Error(UNSUPPORTED_FORMAT(format))
+  }
+
+  return serialized
+}
+
+export function restore<T extends PropertiesSchema> (format: PersistenceFormat = 'binary', data: string | Buffer): Lyra<T> {
+  const db = create({
+    schema: {
+      __placeholder: 'string'
+    }
+  })
+  let deserialized: any
+
+  switch (format) {
+    case 'json':
+      deserialized = JSON.parse(data.toString())
+      break
+    case 'dpack':
+      deserialized = parse(data)
+      break
+    case 'binary':
+      deserialized = decode((data as Buffer).buffer)
+      break
+    default:
+      throw new Error(UNSUPPORTED_FORMAT(format))
+  }
+
+  db.index = deserialized.index
+  db.docs = deserialized.docs
+  db.nodes = deserialized.nodes
+  db.schema = deserialized.schema
+  return db as unknown as Lyra<T>
 }
--- src/node-bun/index.ts
+++ src/node-bun/index.ts
@@ -1,35 +1,15 @@
-import type { PersistenceFormat, AvailableRuntimes } from '../common/types'
+import type { PersistenceFormat } from '../common/types'
 import { readFileSync, writeFileSync } from 'fs'
 import { join } from 'path'
-// @ts-expect-error dpack does not expose types
-import { serialize, parse } from 'dpack'
-import { encode, decode } from '@msgpack/msgpack'
-import { create, save, Lyra, PropertiesSchema } from '@nearform/lyra'
-import { UNSUPPORTED_FORMAT } from '../common/errors'
-import { getDefaultFileName } from '../common/utils'
+import { Lyra, PropertiesSchema } from '@nearform/lyra'
+import { getDefaultFileName, persist as persistDB, restore as restoreDB } from '../common/utils'
 
 function getDefaultOutputDir (format: PersistenceFormat): string {
   return join(process.cwd(), getDefaultFileName(format, 'node'))
 }
 
 export function persist<T extends PropertiesSchema> (db: Lyra<T>, format: PersistenceFormat = 'binary', path: string = getDefaultOutputDir(format)): string {
-  const dbExport = save(db)
-  let serialized: string | Buffer
-
-  switch (format) {
-    case 'json':
-      serialized = JSON.stringify(dbExport)
-      break
-    case 'dpack':
-      serialized = serialize(dbExport)
-      break
-    case 'binary':
-      const msgpack = encode(dbExport)
-      serialized = Buffer.from(msgpack.buffer, msgpack.byteOffset, msgpack.byteLength)
-      break
-    default:
-      throw new Error(UNSUPPORTED_FORMAT(format))
-  }
+  const serialized = persistDB(db, format)
 
   writeFileSync(path, serialized)
 
@@ -37,31 +17,6 @@ export function persist<T extends PropertiesSchema> (db: Lyra<T>, format: Persis
 }
 
 export function restore<T extends PropertiesSchema> (format: PersistenceFormat = 'binary', path: string = getDefaultOutputDir(format)): Lyra<T> {
-  const db = create({
-    schema: {
-      __placeholder: 'string'
-    }
-  })
   const data = readFileSync(path)
-  let deserialized: any
-
-  switch (format) {
-    case 'json':
-      deserialized = JSON.parse(data.toString())
-      break
-    case 'dpack':
-      deserialized = parse(data)
-      break
-    case 'binary':
-      deserialized = decode(data.buffer)
-      break
-    default:
-      throw new Error(UNSUPPORTED_FORMAT(format))
-  }
-
-  db.index = deserialized.index
-  db.docs = deserialized.docs
-  db.nodes = deserialized.nodes
-  db.schema = deserialized.schema
-  return db as unknown as Lyra<T>
+  return restoreDB(format, data)
 }
